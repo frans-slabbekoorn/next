dad1eebfef1eb76d432a581b3a0badda4db4977a
Frans Slabbekoorn <73906909+frans-slabbekoorn@users.noreply.github.com>
2022-11-27 21:02:45 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+// Package imports
+import React from 'react';
+import { NextPage } from 'next';
+
+// Component imports
+import Component from '@components/Component';
+
+const Page: NextPage = () => {
+    return (
+        <div>
+            <Component />
+        </div>
+    );
+};
+
+export default Page;
